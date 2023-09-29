d06aa4e28bd67982f63fcdbb577f9555b1d5f191
rajat455 <rajatjayswal80@gmail.com>
2023-09-29 19:30:07 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const cors = require("cors");
+const { message } = require('statuses');
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
+const users = [{}];
+
+io.on("connection", (socket) => {
+  console.log("New Connection");
+
+
+  socket.on('joined', ({ user }) => {
+    users[socket.id] = user;
+    console.log(user)
+    console.log(`${user} has joined `);
+    socket.broadcast.emit('userJoined', { user: "Admin", message: ` ${users[socket.id]} has joined` });
+    socket.emit('welcome', { user: "Admin", message: `Welcome to the chat,${users[socket.id]} ` })
+  })
+
+  socket.on('message', ({ message, id }) => {
+    io.emit('sendMessage', { user: users[id], message, id });
+  })
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('leave', { user: "Admin", message: `${users[socket.id]}  has left` });
+    console.log(`user left`);
+  })
+});
+
+app.get("/", (req, res) => {
+  return res.status(200).send({ message: "Success" })
+})
+
+server.listen(5500, () => {
+  console.log("server started")
+})
