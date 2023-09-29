4e82fd8442a8fab66ead496e33882f9895fccac4
rajat455 <rajatjayswal80@gmail.com>
2023-09-29 19:50:18 +0530

    after app.use set cors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { message } = require('statuses');
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin:"http://localhost:3000"}))
 
 const server = http.createServer(app);
 const io = socketIO(server, {
