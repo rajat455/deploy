128873e2a266e1e5ac9e5cc5bf6629e47e70bc79
rajat455 <rajatjayswal80@gmail.com>
2023-09-30 11:33:13 +0530

    after log

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use((req,res,next) => {
+  console.log("Request")
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE")
   res.header(
